ed94e0c6638b40188c02eb53cd4545568e425649
DollofCuty <chr.unterrainer@gmail.com>
2019-04-05 01:08:33 +0200

    changed the way the weights are loaded

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source venv/bin/activate
 cd webapp
+
 exec gunicorn -c static/conf/gunicorn_config.py --access-logfile - --error-logfile - main:app
--- webapp/email_service.py
+++ webapp/email_service.py
@@ -1,7 +1,7 @@
 #!/venv/bin python
 
 import os
-from flask import Flask, Blueprint, render_template, flash, request
+from flask import Flask, Blueprint, render_template, flash, request, current_app
 from flask_mail import Mail, Message
 
 from forms import EmailForm
@@ -17,12 +17,13 @@ def send():
     if form.validate():
         app.config.from_object('email_conf.Config')
         mail = Mail(app)
-        msg = Message(sender = app.config['MAIL_USERNAME'], recipients = [app.config['MAIL_USERNAME']], subject= f'message from {form.name.data} through website')
+        msg = Message(sender = current_app.config['MAIL_USERNAME'], recipients = [current_app.config['MAIL_USERNAME']], 
+                      subject= f'message from {form.name.data} through website')
         mail.body = form.message.data
         msg.html = f'{form.email_field.data}<br>{mail.body}'
         mail.send(msg)
         flash('Message sent', 'success')
-        return render_template("home.html", form=form, mail=app.config['MAIL_USERNAME'])
+        return render_template("home.html", form=form, mail=current_app.config['MAIL_USERNAME'])
     
     flash('There was an error sending the message', 'error')
-    return render_template("home.html", form=form, mail=app.config['MAIL_USERNAME'])
+    return render_template("home.html", form=form, mail=current_app.config['MAIL_USERNAME'])
